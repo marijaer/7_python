280359b49ce2e571f795696e32d74da0b470b35d
Marija Erceg <marija.erceg1@gmail.com>
2020-11-05 09:54:25 +0100

    initial commit

---
+++ mood.py
@@ -0,0 +1,19 @@
+x=input("How are you feeling today? :")
+
+if x == "happy":
+    print("It is great to see you happy!")
+
+elif x == "nervous":
+    print("Take a deep breath three times!")
+
+elif x == "sad":
+    print("Read some books!")
+
+elif x == "excited":
+    print("Wow, tell me more about it!")
+
+elif x == "relaxed":
+    print("I am glad to see those three deep breaths worked!")
+
+else:
+    print("Sorry, I don`t recognise this mood!")
